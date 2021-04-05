701ed69133c7a43a6c21d90b8e6dc56b89886d1a
Ishika Jain <ij36@cornell.edu>
2021-04-05 16:12:28 -0400

    added netids

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Itinerary Planner"
+net_id = "Sophie Keller: slk262, Jordana Socher: jns92, Ishika Jain: ij36,  Samantha Meakem: sam458, Nithish Kalpat: nk456 "
 
 @irsystem.route('/', methods=['GET'])
 def search():
