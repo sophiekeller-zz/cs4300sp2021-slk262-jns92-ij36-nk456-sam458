4396777205c36bfad6f8982a745277e3b50bcae7
sophiekeller <skeller2022@gmail.com>
2021-04-13 16:28:38 -0400

    added datasets

---
+++ app/irsystem/models/explore.py
@@ -0,0 +1,83 @@
+import jsonlines
+import json
+import matplotlib.pyplot as plt
+
+dirty_words = 0
+clean_words = 0
+loud_words = 0
+quiet_words = 0
+positive_words = 0
+negative_words = 0
+water_words = 0
+
+words = {
+  "dirty": ["stink", "stinks", "smells", "stinky", "rotten", "disgusting", "gross","nasty", "worn", "dirty"],
+  "clean": ["flawless", "clean", "clear", "comfortable", "comfortably"], 
+  "loud": ["loud", "noise", "noisy"],
+  "quiet": ["quiet"], 
+  #"positive": ["good", "nice", "kind", "perfect", "great", "lovely", "beautiful", "perfect", "amazing", "brilliant", "incredible", "fantastic", "pleasant", "wonderful", "comfortable", "best", "bright", "greatest"],
+  "negative": ["bad", "worst", "wait", "unpleasant"],
+  "swimming": ["sauna", "pool", "spa", "swim", "swimming", "ocean", "lake"],
+  "nature": ["garden", "park", "river", "lake", "sunset"],
+  "expensive": ["pricey", "pricy", "expensive"],
+  "inexpensive": ["cheap", "inexpensive", "low-cost", "bargain", "affordable"],
+  "drinking": ["bar", "pub"]
+}
+
+words = {
+  "good taste": ["delicious", "delectable", "fresh", "tasty", "yum", "yummy", "flavorful", "divine", "good", "nice", "kind", "perfect", "great", "lovely", "beautiful", "perfect", "amazing", "brilliant", "incredible", "fantastic", "pleasant", "wonderful", "comfortable", "best", "bright", "greatest"],
+  "bad taste": ["bad", "worst", "wait", "unpleasant", "gross", "dirty", "yuck", "dry", "under", "raw", "undercooked", "rot", "rotten"],
+  "expensive": ["pricey", "pricy", "expensive"],
+  "inexpensive": ["cheap", "inexpensive", "low-cost", "bargain", "affordable"],
+  "drinking": ["bar", "pub", "wine", "beer", "shot", "cocktail", "cocktails", "beers"],
+  "ambiance": ["setting", "ambiance", "fancy", "pretty", "decor", "decorated", "creative", "vibe", "cute", "vintage"],
+  "music": ["music", "pop", "rock", "reggae", "jazz", "band", "performance"]
+}
+
+words = {
+  "expensive": ["pricey", "pricy", "expensive"],
+  "inexpensive": ["cheap", "inexpensive", "low-cost", "bargain", "affordable"],
+  "drinking": ["bar", "pub", "wine", "beer", "shot", "cocktail", "cocktails", "beers"],
+  "ambiance": ["setting", "ambiance", "fancy", "pretty", "decor", "decorated", "creative", "vibe", "cute", "vintage"],
+  "music": ["music", "pop", "rock", "reggae", "jazz", "band", "performance", "violin", "piano"],
+  "sports": ["active", "basketball", "soccer", "tennis", "baseball", "football", "skate", "skating", "lacrosse", "cricket", "rugby", "squash", "yoga", "exercise", "bike", "biking"],
+  "water activities": ["pool", "lake", "river", "stream", "ocean", "sea", "beach", "sailing", "sail", "boat", "swim", "swimming"]
+
+}
+
+counts = {category: 0 for category in words}
+
+for city in ["london","amsterdam", "barcelona", "berlin", "dubai"]:
+    for category in ["accommodation", "restaurant", "attraction"]:
+        with jsonlines.open(f"jsonl-files/{city}-{category}.jsonl") as f:
+          for line in f.iter():
+            tokens = []
+            for r in line["reviews"]:
+              tokens += [x.lower() for x in r["text"].split(" ")]
+            line["reviews_tokenized"] = tokens
+            with open(f'tokenized-files/{city}-{category}.jsonl', 'a') as outfile:
+              json.dump(line, outfile) 
+              outfile.write("\n")
+
+# with jsonlines.open('london-attraction.jsonl') as f:
+#     line_count = 0
+#     for line in f.iter():
+#       reviews = line["reviews"]
+#       for category in words:
+#         for word in words[category]:
+#           if word in reviews:
+#             counts[category] += 1
+#       line_count += 1
+
+# pairs = counts.items()
+# keys = [x[0] for x in pairs]
+# vals = [x[1] for x in pairs]
+# plt.title("London Attraction Review Categories")
+# plt.ylabel('Amounts')
+# plt.xlabel('Word Category')
+# plt.bar(keys, vals)
+# plt.show()
+
+# print(f"{line_count} hotels reviewed")
+# for category in counts:
+#   print(f"{counts[category]} hotels received reviews corresponding to {category}")
---
+++ app/irsystem/models/parse.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import csv
+
+
+# places = requests.get(
+#     f"http://tour-pedia.org/api/getPlaces?category=restaurant&location={city}").json()
+
+
+
+for city in ["london","amsterdam", "barcelona", "berlin", "dubai"]:
+    for category in ["accommodation", "restaurant", "attraction"]:
+        print(f"starting {category}")
+        with open(f"tourpedia_files/{city}-{category}.csv") as csvfile:
+            csvReader = csv.DictReader(csvfile)
+            for row in csvReader:
+                if row["reviews"] is not None:
+                    place = {"name": row["name"],"lat":row["lat"], "lng":row["lng"], "address": row["address"]}
+                    if "subCategory" in row:
+                        place["subCategory"] = row["subCategory"]
+                    try: 
+                        reviews = requests.get(row["reviews"]).json()
+                        pared_reviews = []
+                        for r in reviews:
+                            pared_reviews.append({"language": r["language"], "rating": r["rating"], "text":r["text"]})
+                        if pared_reviews:
+                            place["reviews"] = pared_reviews
+                            with open(f'{city}-{category}.jsonl', 'a') as outfile:
+                                json.dump(place, outfile) 
+                                outfile.write("\n")
+                    except Exception as e:
+                        pass
+                
+        
+
