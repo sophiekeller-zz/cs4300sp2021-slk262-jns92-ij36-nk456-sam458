e2985e5d0c85255b44b3016223d7a5b70334df7a
sophiekeller <skeller2022@gmail.com>
2021-04-17 13:12:39 -0400

    added back files

--- app/irsystem/models/tourpedia_data_structs.py
+++ app/irsystem/models/tourpedia_data_structs.py
@@ -1,8 +1,9 @@
-# import jsonlines
+import jsonlines
 import numpy as np
 import math
 import json
 city_count = {}
+city_coords = {}
 
 accommodation_words = {
   "dirty": ["stink", "stinks", "smells", "stinky", "rotten", "disgusting", "gross","nasty", "worn", "dirty"],
@@ -28,31 +29,35 @@ restaurant_words = {
   "music": ["music", "pop", "rock", "reggae", "jazz", "band", "performance"]
 }
 
-# cities = ["berlin", "barcelona", "dubai", "london", "amsterdam"]
-# maps = ["restaurant", "accommodation"]
-# words = {"restaurant": restaurant_words, "accommodation": accommodation_words}
-# ind = 0
-# for c in cities:
-#     city_count[c] = {}
-#     for m in maps:
-#         json_string = 'tokenized-files/'+c+'-'+m+'.jsonl'
-#         with jsonlines.open(json_string) as f:
-#             line_count = 0
-#             mappings = {}
-#             for line in f.iter():
-#                 name = line["name"]
-#                 reviews_tokenized = line["reviews_tokenized"]
-#                 count_dict = {}
-#                 for category in words[m]:
-#                     for word in words[m][category]:
-#                         if word in reviews_tokenized:
-#                             if category in count_dict:
-#                                 count_dict[category] += 1
-#                             else:
-#                                 count_dict[category] = 1
-#                 mappings[name] = count_dict
-#                 line_count += 1
-#         city_count[c][m] = mappings
+cities = ["berlin", "barcelona", "dubai", "london", "amsterdam"]
+maps = ["restaurant", "accommodation"]
+words = {"restaurant": restaurant_words, "accommodation": accommodation_words}
+ind = 0
+for c in cities:
+    city_count[c] = {}
+    city_coords[c] = {}
+    for m in maps:
+        json_string = 'tokenized-files/'+c+'-'+m+'.jsonl'
+        with jsonlines.open(json_string) as f:
+            line_count = 0
+            mappings = {}
+            coords = {}
+            for line in f.iter():
+                name = line["name"]
+                reviews_tokenized = line["reviews_tokenized"]
+                count_dict = {}
+                for category in words[m]:
+                    for word in words[m][category]:
+                        if word in reviews_tokenized:
+                            if category in count_dict:
+                                count_dict[category] += 1
+                            else:
+                                count_dict[category] = 1
+                mappings[name] = count_dict
+                coords[name] = {"lat": line["lat"], "lng": line["lng"]}
+                line_count += 1
+        city_coords[c][m] = coords
+        city_count[c][m] = mappings
     
 # with open('mappings.json', 'a') as outfile:
 #     json.dump(city_count, outfile) 
@@ -69,15 +74,16 @@ for c in cities:
             ind += 1
         city_ind[c][m] = index
 
-with open('inverted-index.json', 'a') as outfile:
-    json.dump(city_ind, outfile) 
+# with open('inverted-index.json', 'a') as outfile:
+#     json.dump(city_ind, outfile) 
 
 #formula found @ http://www.movable-type.co.uk/scripts/latlong.html
 def distance_between(lat1, lat2, long1, long2):
-    phi1 = lat1 * np.math.pi / 180 #radians
-    phi2 = lat2 * np.math.pi / 180
-    change_lat = (lat2 - lat1) * np.math.pi / 180
-    change_long = (long2 - long1) * np.math.pi / 180
+    lat1, lat2, long1, long2 = float(lat1), float(lat2), float(long1), float(long2)
+    phi1 = lat1 * math.pi / 180 #radians
+    phi2 = lat2 * math.pi / 180
+    change_lat = (lat2 - lat1) * math.pi / 180
+    change_long = (long2 - long1) * math.pi / 180
     a = math.sin(change_lat / 2) * math.sin(change_lat / 2) + math.cos(phi1) * \
         math.cos(phi2) * math.sin(change_long / 2) * math.sin(change_long / 2)
     dist = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
@@ -85,16 +91,18 @@ def distance_between(lat1, lat2, long1, long2):
 
 
 #distance matrices
-# city_dist = {}
-# for c in cities:
-#     city_count[c] = {}
-#     mat = np.zeros((city_count[c]["restaurant"].length, city_count[c]["accommodation"].length))
-#     for r in city_count[c]["restaurant"]:
-#         for a in city_count[c]["accommodation"]:
-#             ind_r = city_ind[c][r]
-#             ind_a = city_ind[c][a]
-#             mat[ind_r][ind_a] = distance_between(city_count[c]['restaurant'][r]["lat"],
-#                                                  city_count[c]['accommodation'][a]["lat"],
-#                                                  city_count[c]['restaurant'][r]["lng"],
-#                                                  city_count[c]['accommodation'][a]["lng"])
+city_dist = {}
+for c in cities:
+    mat = np.zeros((len(city_count[c]["restaurant"]), len(city_count[c]["accommodation"])))
+    for r in city_count[c]["restaurant"]:
+        for a in city_count[c]["accommodation"]:
+            ind_r = city_ind[c]["restaurant"][r]
+            ind_a = city_ind[c]["accommodation"][a]
+            mat[ind_r][ind_a] = distance_between(city_coords[c]['restaurant'][r]["lat"],
+                                                 city_coords[c]['accommodation'][a]["lat"],
+                                                 city_coords[c]['restaurant'][r]["lng"],
+                                                 city_coords[c]['accommodation'][a]["lng"])
+    city_dist[c] = mat.tolist()
 
+with open('distance-matrices.json', 'a') as outfile:
+    json.dump(city_dist, outfile) 
